088b2d98fbb3a3f60e101ac0bf2fba50cba0cc1a
Alexander Oiling <alexander.oiling@gmail.com>
2026-03-18 16:03:43 +0200

    Created gavel endpoints for using gavel in conjunction with Junction platform

--- gavel/controllers/api.py
+++ gavel/controllers/api.py
@@ -1,7 +1,7 @@
 from gavel import app
 from gavel.models import *
 import gavel.utils as utils
-from flask import Response, url_for
+from flask import Response, request, url_for
 import json
 
 @app.route('/api/items.csv')
@@ -46,6 +46,53 @@ def decisions_dump():
         str(d.time)
     ] for d in decisions]
     return Response(utils.data_to_csv_string(data), mimetype='text/csv')
+
+@app.route('/api/items', methods=['POST'])
+@utils.protected_endpoint
+def item_import():
+    data = request.get_json()
+    if not isinstance(data, list):
+        return Response(json.dumps({'error': 'Expected a JSON array'}), 400, mimetype='application/json')
+    for i, row in enumerate(data):
+        for field in ('name', 'location', 'description'):
+            if not row.get(field):
+                return Response(json.dumps({'error': 'Row %d missing field: %s' % (i + 1, field)}), 400, mimetype='application/json')
+    created = []
+    def tx():
+        for row in data:
+            item = Item(row['name'], row['location'], row['description'])
+            db.session.add(item)
+            created.append(item)
+        db.session.commit()
+    with_retries(tx)
+    return Response(json.dumps([{'id': i.id, 'name': i.name} for i in created]), 201, mimetype='application/json')
+
+
+@app.route('/api/annotators', methods=['POST'])
+@utils.protected_endpoint
+def annotator_import():
+    data = request.get_json()
+    if not isinstance(data, list):
+        return Response(json.dumps({'error': 'Expected a JSON array'}), 400, mimetype='application/json')
+    for i, row in enumerate(data):
+        for field in ('name', 'email', 'description'):
+            if not row.get(field):
+                return Response(json.dumps({'error': 'Row %d missing field: %s' % (i + 1, field)}), 400, mimetype='application/json')
+    created = []
+    def tx():
+        for row in data:
+            annotator = Annotator(row['name'], row['email'], row['description'])
+            db.session.add(annotator)
+            created.append(annotator)
+        db.session.commit()
+    with_retries(tx)
+    return Response(json.dumps([{
+        'id': a.id,
+        'name': a.name,
+        'email': a.email,
+        'login_link': url_for('login', secret=a.secret, _external=True)
+    } for a in created]), 201, mimetype='application/json')
+
 
 # Authorization Basic username:password
 @app.route('/api/users-link')
